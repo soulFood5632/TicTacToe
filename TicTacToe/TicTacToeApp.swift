8f2399992afcf7947d876a47c23b7dda76ca6519
soulFood5632 <loganu@shaw.ca>
2023-01-04 11:46:45 -0800

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Logan Underwood on 2023-01-04.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
