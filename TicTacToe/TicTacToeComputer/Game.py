ec9c94bb1f257749c1e2905979b1928747e00343
soulFood5632 <loganu@shaw.ca>
2023-01-04 19:59:34 -0800

    pyhton's lack of type safety drives my bananas

--- TicTacToe/TicTacToeComputer/Game.py
+++ TicTacToe/TicTacToeComputer/Game.py
@@ -2,13 +2,11 @@ from enum import Enum
 
 
 class TicTacToe:
-    board = None
-    moveMapX = None
-    moveMapO = None
 
     def __init__(self):
         self.board = Board()
-        self.moveMapX = map(list, int)
+        self.moveMapX = dict()
+        self.moveMapO = dict()
 
     def make_move(self, move, player):
         if player == GamePiece.X:
@@ -21,14 +19,22 @@ class TicTacToe:
     def get_valid_moves(self):
         return self.board.get_valid_moves()
 
+    def is_game_over(self):
+        return self.board.is_game_over()
 
+    def __who_won(self):
+        return self.board.who_won()
+
+    # Only to be called when the game is complete
+    def get_moves(self, game_piece):
+        if game_piece == GamePiece.X:
+            return self.moveMapX
+        else:
+            return self.moveMapO
 
 
 
 class Board:
-
-
-
 
     def __init__(self):
         self.grid = list()
@@ -62,12 +68,27 @@ class Board:
         return returnList
 
     def is_game_over(self):
+        if self.__is_full():
+            return True
         for triplets in self.__winnerList:
+            if self.grid[triplets[0]] == self.grid[triplets[1]] or self.grid[triplets[1]] == self.grid[triplets[2]] and self.grid[triplets[0]] != GamePiece.NO:
+                return True
+        return False
 
+    def __is_full(self):
+        for val in self.grid:
+            if val.NO:
+                return False
+        return True
 
-
-
-
+    def who_won(self):
+        if self.__is_full():
+            for triplets in self.__winnerList:
+                if (self.grid[triplets[0]] and self.grid[triplets[1]] and self.grid[triplets[2]]) == GamePiece.X:
+                    return GamePiece.X
+                if (self.grid[triplets[0]] and self.grid[triplets[1]] and self.grid[triplets[2]]) == GamePiece.O:
+                    return GamePiece.O
+            return GamePiece.NO
 
 
 class GamePiece(Enum):
